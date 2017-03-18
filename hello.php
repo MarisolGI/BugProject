ca2db8c33869ba80dc339d46e28eeb67d4a517a8
MarisolGI <marisol.garcia.ituarte@gmail.com>
2017-03-18 14:17:52 +0100

    pop up and table

--- hello.php
+++ hello.php
@@ -3,11 +3,86 @@
 	$show = show_hello();
 ?>
 <html>
+
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+
+
 <head>
 <title>PHP Test</title>
+
+<script type="text/javascript" src="js/jqBootstrapValidation.js"></script>
+  <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+    <!-- Contact Form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Theme JavaScript -->
+    <script src="js/freelancer.min.js"></script>
+
 </head>
 <body>
 <?php echo '<p>Our first project test</p>'; ?>
 <?php echo $show?>
+
+
+<table>
+  <tr>
+    <th>Month</th>
+    <th>Savings</th>
+    <th>Test</th>
+  </tr>
+  <tr>
+    <td>January</td>
+    <td>$100</td>
+    <td>$100</td>
+  </tr>
+ 
+  <tr>
+    <td>January</td>
+    <td>$100</td>
+    <td>$100</td>
+  </tr>
+</table>
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap">Open modal for @getbootstrap</button>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-group">
+            <label for="recipient-name" class="form-control-label">Recipient:</label>
+            <input type="text" class="form-control" id="recipient-name">
+          </div>
+          <div class="form-group">
+            <label for="message-text" class="form-control-label">Message:</label>
+            <textarea class="form-control" id="message-text"></textarea>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send message</button>
+	  </div>
+	  </div>
+	  </div>
+	  </div>
+	  
+	
  </body>
 </html>
