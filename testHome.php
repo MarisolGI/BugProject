c6729f9ff10cc949268469c28b5155daa90ebba3
MarisolGI <marisol.garcia.ituarte@gmail.com>
2017-06-05 18:28:46 +0200

    Navigation Bar

---
+++ testHome.php
@@ -0,0 +1,42 @@
+
+
+<html>
+
+
+<head>
+<title>PHP Test</title>
+
+<!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">  
+  
+
+
+
+</head>
+
+<body >
+
+ 
+ <div style="border : solid 1px #fff; background : #fff; color : #000; padding : 0px; width:200px; height : 75px; overflow : auto; ">
+<?php include("menu.html"); ?>
+</div>
+
+
+<div>
+test
+</div>
+
+</body>
+
+</html>
+
+
+
